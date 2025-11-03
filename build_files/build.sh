a290202e07f87bcbbcde4350fab900fea03ace1e
Mathias Ringhof <mathias@ringhof.eu>
2025-11-03 20:47:53 +0100

    /usr/local also part of /var - target /usr/bin

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,4 @@
 
 set -ouex pipefail
 
-ls -ld /usr
-ls -ld /usr/local
-ls -ld /usr/local/bin
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_SKIP_START=true INSTALL_K3S_BIN_DIR=/usr/bin sh -
